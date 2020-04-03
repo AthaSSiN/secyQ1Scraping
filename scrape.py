b2d77c29e4e6073a322b528dd22e0728d0d9cf7a
Atharv Singh Patlan <56124496+AthaSSiN@users.noreply.github.com>
2020-04-03 10:41:55 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,6 +7,7 @@ records = []
 for i in range(12):
   
     URL = f"https://summerofcode.withgoogle.com/archive/2019/projects/?page={i+1}"
+    print(f'Reading from URL : {URL}')
     r = requests.get(URL) 
     soup = BeautifulSoup(r.content, 'html5lib')
     
@@ -21,9 +22,13 @@ for i in range(12):
         record['proj'] = vals[2].text
         records.append(record)
 
+print("Scraping over... Writing information to result.csv")
+        
 filename = 'result.csv'
 with open(filename, 'w') as f: 
     w = csv.DictWriter(f,['name', 'org', 'proj']) 
     w.writeheader() 
     for record in records: 
         w.writerow(record) 
+        
+print("Records written")
